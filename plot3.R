55d341364a480bce7c05845c5d4bb27373cb9abe
gtscientist <35358057+gtscientist@users.noreply.github.com>
2018-03-05 08:26:00 -0500

    Changes from points to lines

--- plot3.R
+++ plot3.R
@@ -24,8 +24,8 @@ plot3 <- function(output = 'png') {
         
         # create the multi-data line graph
         with(twodays, plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
-        with(twodays, points(DateTime, Sub_metering_2, col = "red", type = "l"))
-        with(twodays, points(DateTime, Sub_metering_3, col = "blue", type = "l"))
+        with(twodays, lines(DateTime, Sub_metering_2, col = "red", type = "l"))
+        with(twodays, lines(DateTime, Sub_metering_3, col = "blue", type = "l"))
         legend("topright", lty = 1, lwd = 2, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
         
         # if the output is png, close the png graphics device we opened earlier
